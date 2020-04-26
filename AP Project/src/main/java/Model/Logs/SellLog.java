6eed44ecc0dfb7c547c921ab9e0cd317f601a3df
Amir-NM <59971778+Amir-NM@users.noreply.github.com>
2020-04-26 05:44:46 -0700

    Add files via upload

--- AP Project/src/main/java/Model/Logs/SellLog.java
+++ AP Project/src/main/java/Model/Logs/SellLog.java
@@ -1,4 +1,18 @@
 package Model.Logs;
 
+import Model.Product;
+
+import java.util.GregorianCalendar;
+
 public class SellLog {
+    private long sellLogId;
+    private GregorianCalendar date;
+    private int receivedMoney;
+    private int offPercent;
+    private Product soldProduct;
+    private String buyerUsername;
+    private String state;
+    private String receiverName;
+    private String receiverPhoneNumber;
+    private String receiverAddress;
 }
