ecda1652585cb5821debde55a5608a4842e71025
sahm124 <63345017+sahm124@users.noreply.github.com>
2020-05-01 04:13:28 -0400

    Update CreateOffRequest.java

--- AP Project/src/main/java/Model/Requests/CreateOffRequest.java
+++ AP Project/src/main/java/Model/Requests/CreateOffRequest.java
@@ -1,4 +1,65 @@
 package Model.Requests;
+import java.util.ArrayList;
+import java.util.Date;
 
-public class CreateOffRequest extends Request {
+public class CrerateOffRequest extends Request {
+
+    public CrerateOffRequest(ArrayList<Request> unAnsweredRequests, ArrayList<Request> answeredRequests, int requestId, Date requestDate, Date answerDate, Manager responsiveManager, RequestState requestState) {
+        super(unAnsweredRequests, answeredRequests, requestId, requestDate, answerDate, responsiveManager, requestState);
+    }
+
+    boolean forEdit;
+    Off privious;
+    ArrayList<Product> effectingProducts;
+    Date end;
+    int offPercentage;
+
+    public CrerateOffRequest(ArrayList<Request> unAnsweredRequests, ArrayList<Request> answeredRequests, int requestId, Date requestDate, Date answerDate, Manager responsiveManager, RequestState requestState, boolean forEdit, Off privious, ArrayList<Product> effectingProducts, Date end, int offPercentage) {
+        super(unAnsweredRequests, answeredRequests, requestId, requestDate, answerDate, responsiveManager, requestState);
+        this.forEdit = forEdit;
+        this.privious = privious;
+        this.effectingProducts = effectingProducts;
+        this.end = end;
+        this.offPercentage = offPercentage;
+    }
+
+    public boolean isForEdit() {
+        return forEdit;
+    }
+
+    public void setForEdit(boolean forEdit) {
+        this.forEdit = forEdit;
+    }
+
+    public Off getPrivious() {
+        return privious;
+    }
+
+    public void setPrivious(Off privious) {
+        this.privious = privious;
+    }
+
+    public ArrayList<Product> getEffectingProducts() {
+        return effectingProducts;
+    }
+
+    public void setEffectingProducts(ArrayList<Product> effectingProducts) {
+        this.effectingProducts = effectingProducts;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public int getOffPercentage() {
+        return offPercentage;
+    }
+
+    public void setOffPercentage(int offPercentage) {
+        this.offPercentage = offPercentage;
+    }
 }
