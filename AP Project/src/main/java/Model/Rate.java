4ce52715cbbf71aae89d5053b9928d82b793a959
sahm124 <63345017+sahm124@users.noreply.github.com>
2020-05-01 03:50:49 -0400

    Update Rate.java

--- AP Project/src/main/java/Model/Rate.java
+++ AP Project/src/main/java/Model/Rate.java
@@ -1,4 +1,37 @@
 package Model;
 
 public class Rate {
+    CustomerAccount user;
+    Product product;
+    double rate;
+
+    public rate(CustomerAccount user, Product product, double rate) {
+        this.user = user;
+        this.product = product;
+        this.rate = rate;
+    }
+
+    public CustomerAccount getUser() {
+        return user;
+    }
+
+    public void setUser(CustomerAccount user) {
+        this.user = user;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
 }
