b3eb93c649c7334b6b265d046281b3078cea0cd0
Amir-NM <59971778+Amir-NM@users.noreply.github.com>
2020-05-03 21:33:51 +0430

    Update AllPages.java

--- AP Project/src/main/java/View/AllPages.java
+++ AP Project/src/main/java/View/AllPages.java
@@ -1,4 +1,9 @@
 package View;
 
-public enum AllPages {
+import java.util.Scanner;
+
+public abstract class Page {
+     protected static Scanner scanner = new Scanner(System.in);
+
+    public abstract AllPages run();
 }
