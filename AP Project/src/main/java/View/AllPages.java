68e756c941509039226e23af196e5b7d97e47e2b
Amir-NM <59971778+Amir-NM@users.noreply.github.com>
2020-05-03 21:37:46 +0430

    Update AllPages.java

--- AP Project/src/main/java/View/AllPages.java
+++ AP Project/src/main/java/View/AllPages.java
@@ -1,9 +1,21 @@
 package View;
 
-import java.util.Scanner;
+import View.Pages.*;
+import View.Pages.AccountsPage.*;
 
-public abstract class Page {
-     protected static Scanner scanner = new Scanner(System.in);
+public enum AllPages {
+    LOGIN_REGISTER_PAGE(LoginRegisterPage.getInstance()),
+    CUSTOMER_PAGE(CustomerPage.getInstance()),
+    MANAGER_PAGE(ManagerPage.getInstance()),
+    SELLER_PAGE(SellerPage.getInstance()),
+    ALL_PRODUCTS_PAGE(AllProductsPage.getInstance()),
+    CART_PAGE(CartPage.getInstance()),
+    OFFS_PAGE(OffsPage.getInstance()),
+    PRODUCT_PAGE(ProductPage.getInstance());
 
-    public abstract AllPages run();
+    public Page page;
+
+    AllPages(Page page) {
+        this.page = page;
+    }
 }
