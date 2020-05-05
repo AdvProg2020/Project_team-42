9f44042ea7b4816755f26b18efe6861b3149bb17
amirt098 <62812464+amirt098@users.noreply.github.com>
2020-05-05 17:58:09 +0430

    Update Shop.java

--- AP Project/src/main/java/Model/Shop.java
+++ AP Project/src/main/java/Model/Shop.java
@@ -1,4 +1,17 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Shop {
+  
+    private HashMap<Product,Integer> allProductAndCount;
+    private ArrayList<Product> allProducts;
+    private ArrayList<Product> allProductOnOffs;
+    private ArrayList<SellLog> allSellLogs;
+    private ArrayList<BuyLog> allBuyLogs;
+    private ArrayList<Discount> allDiscounts;
+    private ArrayList<Category> allCategorys;
+    private ArrayList<Off> allOffs;
+  
 }
