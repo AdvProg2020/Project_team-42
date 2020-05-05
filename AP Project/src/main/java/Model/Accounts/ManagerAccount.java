0f3ac263b470f6fe127e3900b068390902114559
amirt098 <62812464+amirt098@users.noreply.github.com>
2020-05-05 17:55:31 +0430

    Update ManagerAccount.java

--- AP Project/src/main/java/Model/Accounts/ManagerAccount.java
+++ AP Project/src/main/java/Model/Accounts/ManagerAccount.java
@@ -1,4 +1,8 @@
 package Model.Accounts;
 
+import java.util.ArrayList;
+
 public class ManagerAccount extends Account {
+   private static ArrayList<ManagerAccount> allManagerAccount = new ArrayList<ManagerAccount>();
+    private boolean isMain;
 }
