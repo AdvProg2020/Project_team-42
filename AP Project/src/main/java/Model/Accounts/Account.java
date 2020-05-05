c47b82b6fca5655c57bc40d067a005ca92d48310
amirt098 <62812464+amirt098@users.noreply.github.com>
2020-05-05 17:54:28 +0430

    Update Account.java

--- AP Project/src/main/java/Model/Accounts/Account.java
+++ AP Project/src/main/java/Model/Accounts/Account.java
@@ -1,4 +1,63 @@
 package Model.Accounts;
+import java.util.ArrayList;
+
 
 public abstract class Account {
+
+    private static ArrayList<Account> allAccounts;
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private String accountType;
+    private double credit;
+
+    public static Account getAccountByUserName(String userName){
+        for (Account account : allAccounts) {
+            if(account.userName.equals(userName))
+            {
+                return account;
+            }
+        }
+        return null;
+    }
+
+    public static Account getphoneNubmberByUserName(String userName){
+        for (Account account : allAccounts) {
+            if(account.userName.equals(userName))
+            {
+                return account;
+            }
+        }
+        return null;
+    }
+
+    public static Account getEmailByUserName(String userName){
+        for (Account account : allAccounts) {
+            if(account.userName.equals(userName))
+            {
+                return account;
+            }
+        }
+        return null;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public double getCredit() {
+        return credit;
+    }
+
+    public void setCredit(double credit) {
+        this.credit = credit;
+    }
+
 }
