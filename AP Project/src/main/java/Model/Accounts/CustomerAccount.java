bbcb73bb33d54959bfb9a2b4ce0f5afc7568ed12
amirt098 <62812464+amirt098@users.noreply.github.com>
2020-05-05 17:52:41 +0430

    Update CustomerAccount.java

--- AP Project/src/main/java/Model/Accounts/CustomerAccount.java
+++ AP Project/src/main/java/Model/Accounts/CustomerAccount.java
@@ -1,4 +1,12 @@
 package Model.Accounts;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CustomerAccount extends Account {
+
+    private static ArrayList<CustomerAccount> allCustomerAccounts = new ArrayList<CustomerAccount>();
+    private ArrayList<Product> cart;
+    private ArrayList<BuyLog> thisCustomerBuyLogs;
+    private HashMap<Integer , String>discountCodeAndUserNumber;
+
 }
