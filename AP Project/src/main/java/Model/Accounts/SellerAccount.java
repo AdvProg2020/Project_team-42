221d45b58ab21fadbd171e8fbd792922c7650941
amirt098 <62812464+amirt098@users.noreply.github.com>
2020-05-05 17:56:51 +0430

    Update SellerAccount.java

--- AP Project/src/main/java/Model/Accounts/SellerAccount.java
+++ AP Project/src/main/java/Model/Accounts/SellerAccount.java
@@ -1,4 +1,15 @@
 package Model.Accounts;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class SellerAccount extends Account {
+  
+    private static ArrayList<SellerAccount> allSellerAccounts = new ArrayList<SellerAccount>();
+    private String componyOrWorkShopName ;
+    private ArrayList<SellLog> thisCostomerAllSellLogs;
+    private HashMap<Product,Integer> sallableProductAndCounts;
+    private ArrayList<Off> offs;
+    private ArrayList<Requests> requests;
+  
 }
