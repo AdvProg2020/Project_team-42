8d2a7032875b55f822d27ea8d43324e59d59c4f4
sahm124 <63345017+sahm124@users.noreply.github.com>
2020-06-13 23:02:48 +0430

    Update Category.java

--- AP Project/src/main/java/Model/Category.java
+++ AP Project/src/main/java/Model/Category.java
@@ -28,5 +28,35 @@ public class Category {
                 ", productsAndCount=" + productsAndCount +
                 '}';
     }
+     public HashMap<Product, Integer> getProductsAndCount() {
+        return productsAndCount;
+    }
+
+    public ArrayList<Category> getSubCategories() {
+        return subCategories;
+    }
+
+    public void setParentCategory(Category parentCategory) {
+        this.parentCategory = parentCategory;
+    }
+    
+     public void editCategory(String newName, String attribute,Category parentCategory)
+    {
+        this.getParentCategory().subCategories.remove(this);
+        this.parentCategory=parentCategory;
+        this.name = name;
+        this.attribute = attribute;
+    }
+    public Category getParentCategory() {
+        return parentCategory;
+    }
+
+    public Category(String name, String attribute, Category parentCategory, ArrayList<Category> subCategories, HashMap<Product, Integer> productsAndCount) {
+        this.name = name;
+        this.attribute = attribute;
+        this.parentCategory = parentCategory;
+        this.subCategories = subCategories;
+        this.productsAndCount = productsAndCount;
+    }
 }
 
