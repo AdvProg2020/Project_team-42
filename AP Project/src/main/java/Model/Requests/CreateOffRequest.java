b505b5528cf7ad2cb2b2df9b0c1bd08d407f0346
amirt098 <62812464+amirt098@users.noreply.github.com>
2020-06-13 22:09:34 +0430

    Update CreateOffRequest.java

--- AP Project/src/main/java/Model/Requests/CreateOffRequest.java
+++ AP Project/src/main/java/Model/Requests/CreateOffRequest.java
@@ -12,4 +12,13 @@ public class CreateOffRequest extends Request {
     private ArrayList<Product> effectingProducts;
     private GregorianCalendar end;
     private int offPercentage;
+    
+     public CreateOffRequest(boolean forEdit, Off previous, ArrayList<Product> effectingProducts, GregorianCalendar start, GregorianCalendar end, double offPercentage) {
+        this.forEdit = forEdit;
+        this.previous = previous;
+        this.effectingProducts = effectingProducts;
+        this.start = start;
+        this.end = end;
+        this.offPercentage = offPercentage;
+    }
 }
