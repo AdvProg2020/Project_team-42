4e9eef06a8105814c6a9dda6406d1ab500d437ac
sahm124 <63345017+sahm124@users.noreply.github.com>
2020-06-13 23:01:08 +0430

    Update Discount.java

--- AP Project/src/main/java/Model/Discount.java
+++ AP Project/src/main/java/Model/Discount.java
@@ -42,4 +42,42 @@ public class Discount {
     public boolean isUsable () {
         return this.begin.getTime().before(new Date()) && this.end.getTime().after(new Date());
     }
+    
+     public HashMap<CustomerAccount, Integer> getEffectingCustomersAndUsageCount() {
+        return effectingCustomersAndUsageCount;
+    }
+
+    public Discount(String discountCode, GregorianCalendar begin, GregorianCalendar end, double discountPercent, int discountAmountLimit, int repeatCountForEachCustomer , HashMap<CustomerAccount, Integer> effectingCustomersAndUsageCount) {
+        this.discountCode = discountCode;
+        this.begin = begin;
+        this.end = end;
+        this.discountPercent = discountPercent;
+        this.discountAmountLimit = discountAmountLimit;
+        this.repeatCountForEachCustomer = repeatCountForEachCustomer;
+        this.effectingCustomersAndUsageCount = effectingCustomersAndUsageCount;
+    }
+
+    public void setDiscountCode(String discountCode) {
+        this.discountCode = discountCode;
+    }
+
+    public void setBegin(GregorianCalendar begin) {
+        this.begin = begin;
+    }
+
+    public void setEnd(GregorianCalendar end) {
+        this.end = end;
+    }
+
+    public void setDiscountPercent(double discountPercent) {
+        this.discountPercent = discountPercent;
+    }
+
+    public void setDiscountAmountLimit(int discountAmountLimit) {
+        this.discountAmountLimit = discountAmountLimit;
+    }
+
+    public void setRepeatCountForEachCustomer(int repeatCountForEachCustomer) {
+        this.repeatCountForEachCustomer = repeatCountForEachCustomer;
+    }
 }
