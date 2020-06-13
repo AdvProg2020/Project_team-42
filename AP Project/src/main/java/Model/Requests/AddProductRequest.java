27bf08ca28f59fe5c2cc2fc72bb15238debb32b0
sahm124 <63345017+sahm124@users.noreply.github.com>
2020-06-13 22:59:02 +0430

    Update AddProductRequest.java

--- AP Project/src/main/java/Model/Requests/AddProductRequest.java
+++ AP Project/src/main/java/Model/Requests/AddProductRequest.java
@@ -3,6 +3,56 @@ package Model.Requests;
 import Model.Accounts.SellerAccount;
 import Model.Category;
 
+public SellerAccount getSeller() {
+        return seller;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getAttribute() {
+        return attribute;
+    }
+
+    @Override
+    public String toString() {
+        return "AddProductRequest{" +
+                "seller=" + seller +
+                ", name='" + name + '\'' +
+                ", productId=" + productId +
+                ", count=" + count +
+                ", brand='" + brand + '\'' +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                ", attribute='" + attribute + '\'' +
+                '}';
+    }
+
 public class AddProductRequest extends Request {
     private SellerAccount seller;
     private String name;
